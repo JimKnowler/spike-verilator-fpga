8cbcfe27e344ec2ef0a067ec9e4183345af9f402
Jim Knowler <jim.knowler2@gmail.com>
2020-11-15 10:51:08 +0000

    initial commit

---
+++ counter.v
@@ -0,0 +1,15 @@
+/// @module Counter
+/// @brief  A simple incremental counter
+module Counter(
+    input i_clk,
+    output [15:0] o_value
+);
+
+reg [15:0] r_value;
+
+always @(posedge i_clk)
+begin
+    r_value <= r_value + 1;
+end
+
+assign o_value = r_value;
